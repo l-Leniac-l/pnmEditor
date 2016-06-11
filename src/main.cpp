d3c06376aea321c14dbbed5e12f34334819be756
BlindJoker <raphaelcj4@hotmail.com>
2016-06-11 16:02:56 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,75 @@
 #include <iostream>
 #include <string>
 #include <pnme/defs.h>
+#include <cstdlib>
+#include <cstring>
+
+void usage(char *basename)
+{
+	std::cout << "Uso:" << std::endl;
+
+	std::cout << basename << " <imagem>";
+	std::cout << " lighten <-255..255>, darken <-255..255>, pixelate <1..>, ";
+	std::cout << "mirror, negativate, ,sobel, gaussian, box, laplace";
+	std::cout << ", export <saída> [...]";
+
+	std::cout << std::endl;
+
+	std::cout << "(Na ausência de parâmetros para lighten, darken e pixelate, ";
+	std::cout << "eles serão silenciosamente ignorados.)" << std::endl;
+
+	std::cout << std::endl;
+}
 
 int main(int argc, char** argv)
 {
+	bool color = true;
+	int width, height;
+	unsigned char *r, *g, *b;
+
+	if(argc <= 2)
+	{
+		usage(argv[0]);
+		return -1;
+	}
+
+	std::tie(r, g, b) = loadfile(std::string(argv[1]), width, height, color);
+	if(r == nullptr)
+	{
+		std::cout << "falha ao abrir imagem - arquivo não existe." << std::endl;
+	}
+
+	for(int n = 2; n < argc; n ++)
+	{
+		std::string ar(argv[n]);
+		std::cout << argv[n] << std::endl;
+		if(n + 1 < argc)
+		{
+			if(ar == "lighten")
+			{
+				n ++;
+				lighten(r, width, height, CLAMP(atoi(argv[n]), 0, 255));
+				lighten(g, width, height, CLAMP(atoi(argv[n]), 0, 255));
+				lighten(b, width, height, CLAMP(atoi(argv[n]), 0, 255));
+			} else if(ar == "darken")
+			{
+				n ++;
+				lighten(r, width, height, -1 * CLAMP(atoi(argv[n]), 0, 255));
+				lighten(g, width, height, -1 * CLAMP(atoi(argv[n]), 0, 255));
+				lighten(b, width, height, -1 * CLAMP(atoi(argv[n]), 0, 255));
+			} else if(ar == "pixelate")
+			{
+				n ++;
+				pixelate(r, width, height, atoi(argv[n]));
+				pixelate(g, width, height, atoi(argv[n]));
+				pixelate(b, width, height, atoi(argv[n]));
+			} else if(ar == "ep3")
+			{
+				n ++;
+				exportP3(r, g, b, width, height, std::string(argv[n]));
+			}
+		}
+	}
 
 	return 0;
 }
